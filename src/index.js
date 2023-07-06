61f1d9eb2be1606cff6ab07162fb6e67ff9bd99f
Ezequias <ezequiasmartins@gmail.com>
2023-07-06 18:56:10 -0300

    06072023-1856

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './style/global.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <center>
-    <h1>DeliveryBairro.com</h1>
+    <h1>DeliveryBairro.com | Simples e fácil de usar!</h1>
   </center>
 );
